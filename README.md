81b5ade9a3d78c065b8f22a48647f54321ab7b99
Brian Carroll <brianjcarroll0@gmail.com>
2016-08-10 01:38:58 -0400

    Finished first 5 exercises of HW01

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # haskell-upenn
 A repo of the homework for the UPenn course CIS 194: Introduction to Haskell (Spring 2015)
+
+[Link to course lectures](http://www.seas.upenn.edu/~cis194/lectures.html)
+[Sweet Cheat Sheet](http://cheatsheet.codeslower.com/CheatSheet.pdf)
+
+[HW01](http://www.seas.upenn.edu/~cis194/hw/01-intro.pdf)
---
+++ hw01.hs
@@ -0,0 +1,50 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW01 where
+
+-- Exercise 1 -----------------------------------------
+
+-- Get the last digit from a number
+lastDigit :: Integer -> Integer
+lastDigit x = x `mod` 10
+
+-- Drop the last digit from a number
+dropLastDigit :: Integer -> Integer
+dropLastDigit x = x `div` 10
+
+-- Exercise 2 -----------------------------------------
+
+toRevDigits :: Integer -> [Integer]
+toRevDigits x
+  | x <= 0    = []
+  | otherwise = (lastDigit x) : (toRevDigits $ dropLastDigit x)
+
+-- Exercise 3 -----------------------------------------
+
+-- Double every second number in a list starting on the left.
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x:y:xs) = x : (y * 2) : (doubleEveryOther xs)
+
+-- Exercise 4 -----------------------------------------
+
+-- Calculate the sum of all the digits in every Integer.
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits xs = sum [ if x >= 10 then x - 9 else x | x <- xs]
+
+
+-- Exercise 5 -----------------------------------------
+
+-- Validate a credit card number using the above functions.
+luhn :: Integer -> Bool
+luhn x = (lastDigit $ sumDigits $ doubleEveryOther $ toRevDigits x) == 0
+
+-- Exercise 6 -----------------------------------------
+
+-- Towers of Hanoi for three pegs
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi = undefined
